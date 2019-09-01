d472a5a2c9618b26f33be6aaf4e97652c61e4935
Brian <brayomumo5@gmail.com>
2019-09-01 17:05:52 +0300

    Create profiles class

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField('profile/', null = True)
+    bio = models.TextField()
+    user = models.ForeignKey(User)
+    last_update = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ['-last_update']
+
+    def __str__(self):
+        return self.user.name
+
