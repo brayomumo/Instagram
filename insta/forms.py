d735d8dad98a5ec4f1a58cbf8339398007c32492
Brian <brayomumo5@gmail.com>
2019-09-01 17:58:14 +0300

    Create status and comment form

--- insta/forms.py
+++ insta/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Comment, Image
 
 
 class SignUpForm(UserCreationForm):
@@ -11,3 +12,13 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+
+class NewStatusForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields =['image', 'image_caption']
+
+class NewCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment']    
