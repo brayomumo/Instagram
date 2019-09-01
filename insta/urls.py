772af1be1f1b6d8e47c93b812870d4157520e54d
Brian <brayomumo5@gmail.com>
2019-09-01 18:33:14 +0300

    create urls

--- insta/urls.py
+++ insta/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     url(r'^$', views.index, name="home"),
      url(r'^signup/$', views.signup, name='signup'),
     url(r'^accounts/profile/',  views.profile, name="myProfile"),
+    url(r'^user/(\d+)', views.user_profile, name='userProfile'),
+    url(r'^image/(\d+)', views.single_post, name='singleImage'),
+    url(r'^single_image/likes/(\d+)', views.single_image_like, name="singleImageLike"),
     
       # url(r'^login/$', views.login, name='login'),
     # url(r'^logout/$', views.logout, name='logout'),
