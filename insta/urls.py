613301ee4b4588163ac3c1cafdadd6112889e35c
Brian <brayomumo5@gmail.com>
2019-09-01 14:25:52 +0300

    Signup url

--- insta/urls.py
+++ insta/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^$', views.index, name="home"),
      url(r'^signup/$', views.signup, name='signup'),
-      url(r'^login/$', views.login, name='login'),
+      # url(r'^login/$', views.login, name='login'),
     # url(r'^logout/$', views.logout, name='logout'),
 ]
