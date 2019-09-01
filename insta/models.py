6c7bed78e344be82da8df2b82fd16da96806ae97
Brian <brayomumo5@gmail.com>
2019-09-01 17:08:46 +0300

    Create Images class

--- insta/models.py
+++ insta/models.py
@@ -15,3 +15,16 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.name
 
+class Image(models.Model):
+    image = models.ImageField(upload_to ='image/', null=True)
+    image_name = models.CharField(max_length =30, null=True)
+    image_caption = models.TextField(null=True)
+    user = models.ForeignKey(User, null=True)
+    profile = models.ForeignKey(Profile)
+    likes = models.IntegerField(default=0)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+
+    class Meta:
+        ordering = ['-pub_date']
