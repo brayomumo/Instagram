6da2bcc1941b84c55e28a82fc3ccb95544d6de15
Brian <brayomumo5@gmail.com>
2019-09-01 17:22:19 +0300

    Test class Profile

--- insta/tests.py
+++ insta/tests.py
@@ -1,4 +1,23 @@
 from django.test import TestCase
-from .models import Profile
+from .models import Profile,Image,Comment
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    #Set Up method
+    def setUp(self):
+        self.profile = Profile(profile_photo='yes we can', bio='very awesome')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    #Testing save method
+    def test_save_method(self):
+        self.profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) > 0)
+
+    def test_delete_method(self):
+        self.profile.save_profile()
+        self.profile.delete_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)==0)
